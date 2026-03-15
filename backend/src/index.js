561ca210637033d992c5793b58998c0d2787983c
mabhishek-dev <193374368+mabhishek-dev@users.noreply.github.com>
2026-03-16 00:55:10 +0530

    fix express wildcard route

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,12 +22,12 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
